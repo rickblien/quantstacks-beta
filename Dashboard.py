476f032cb636637c8464c9646adeab761bcc2582
ricklien101616 <77305464+ricklien101616@users.noreply.github.com>
2023-01-15 13:41:19 +0800

    updated Dashboard, Discount Cash Flow, and create Sector Analysis page

--- Dashboard.py
+++ Dashboard.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+# list all rows
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 st.set_page_config(
     page_title="QuantStacks beta",
--- pages/2_Discount_Cash_Flow.py
+++ pages/2_Discount_Cash_Flow.py
@@ -8,35 +8,50 @@ st.write('COST OF EQUITY')
 
 # RISK FREE RATE
 st.write('Risk Free Rate')
-def risk_free_rate(country_option):
-    try:
-        # Scrape Counties 10 years bond rate
-        import pandas as pd
-        countries_10_years_bond_rate = pd.read_html("http://www.worldgovernmentbonds.com/", header=0)[1]
-        countries_10_years_bond_rate
+# def risk_free_rate(country_option):
+#     try:
+# Scrape Counties 10 years bond rate
+import pandas as pd
+countries_10_years_bond_rate = pd.read_html("http://www.worldgovernmentbonds.com/", header=0)[1]
+st.write(countries_10_years_bond_rate)
 
-        # Filter out Selected country 10 years bond
+# Rename column names
+countries_10_years_bond_rate.rename(columns={"Unnamed: 0":"Nan","Unnamed: 1":"country_name","Rating":"rating","10Y Bond":"10y_bond","10Y Bond.1":"10y_bond_1","Bank":"bank", "Spread vs":"spread_vs_german", "Spread vs.1":"spread_vs_usa"},inplace=True)
+st.write(countries_10_years_bond_rate)
 
-        # Calculate: Selected Country Default Spread = Selected Country 10 years bond - USA 10 years bond
+# Get country name list
+country_names_list = countries_10_years_bond_rate['country_name'].tolist()
+st.write(country_names_list)
 
-        # Calculate: Selected Country Risk Free Rate = USA 10 years bond - Selected Country Default Spread
+# Filter out Selected country 10 years bond
 
-    except:
-        pass 
+# Calculate: Selected Country Default Spread = Selected Country 10 years bond - USA 10 years bond
 
+# Calculate: Selected Country Risk Free Rate = USA 10 years bond - Selected Country Default Spread
+
+# except:
+#     pass 
+
+# BETA
 st.write('Beta')
 
-st.write('Equity Risk Premium')
+# IMPLIED EQUITY RISK PREMIUM
+st.write('Implied Equity Risk Premium')
 # Selected Country Risk Premium
 
 # Selected Equity Risk Premium
 
-
+# COST OF DEBT
 st.write('COST OF DEBT')
 
+# AVERAGE YIELD ON DEBT
 st.write('Average Yield on Debt')
 
+# TAX SHIELD
 st.write('Tax Shield')
+
+
+
 
 
 st.header('Terminal Value')
